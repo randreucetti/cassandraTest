57b7e66e4be4af1350707b6fc77e9a11831bfc3c
Ross Andreucetti <ross.andreucetti@newswhip.com>
2016-03-14 17:46:33 +0000

    added cassandra code

--- src/main/java/com/randreucetti/App.java
+++ src/main/java/com/randreucetti/App.java
@@ -1,13 +1,13 @@
 package com.randreucetti;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class App {
+	public static void main(String[] args) {
+		Cluster cluster = Cluster.builder()
+				.addContactPoints("127.0.0.1", "127.0.0.2", "127.0.0.3")
+				.build();
+		Session session = cluster.connect("test");
+	}
 }
--- src/test/java/com/randreucetti/AppTest.java
+++ src/test/java/com/randreucetti/AppTest.java
@@ -1,38 +1,13 @@
 package com.randreucetti;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	public static void main(String[] args) {
+		Cluster cluster = Cluster.builder()
+				.addContactPoints("127.0.0.1", "127.0.0.2", "127.0.0.3")
+				.build();
+		Session session = cluster.connect("test");
+	}
 }
