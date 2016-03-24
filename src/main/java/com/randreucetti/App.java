15c5bea644f8eb39a36760a9a0d845b3af3114af
Ross Andreucetti <ross.andreucetti@newswhip.com>
2016-03-24 16:15:26 +0000

    change to prepate statement

--- src/main/java/com/randreucetti/App.java
+++ src/main/java/com/randreucetti/App.java
@@ -1,9 +1,18 @@
 package com.randreucetti;
 
+import java.math.BigInteger;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 
@@ -11,17 +20,37 @@ public class App {
 
 	public final static Logger log = LoggerFactory.getLogger(App.class);
 
-	public static void main(String[] args) throws InterruptedException {
-		Cluster cluster = Cluster
-				.builder()
-				.addContactPoints("127.0.0.1", "127.0.0.2", "127.0.0.3",
-						"127.0.0.4", "127.0.0.5").build();
+	public static void main(String[] args) throws InterruptedException,
+			UnknownHostException {
+		InetSocketAddress n1 = new InetSocketAddress("192.168.1.184", 15001);
+		InetSocketAddress n2 = new InetSocketAddress("192.168.1.184", 15002);
+		InetSocketAddress n3 = new InetSocketAddress("192.168.1.184", 15003);
+		InetSocketAddress n4 = new InetSocketAddress("192.168.1.184", 15004);
+		InetSocketAddress n5 = new InetSocketAddress("192.168.1.184", 15005);
+		List<InetSocketAddress> addresses = Arrays.asList(n1, n2, n3, n4, n5);
+		Cluster cluster = Cluster.builder()
+				.addContactPointsWithPorts(addresses).build();
 		Session session = cluster.connect("test");
+
+		PreparedStatement statement = session
+				.prepare("INSERT INTO emp (emp_id, emp_city, emp_name, emp_phone, emp_sal VALUES (?, ?, ?, ?, ?)");
+		Random r = new Random();
+		SessionIdentifierGenerator stringGenerator = new SessionIdentifierGenerator();
 		while (true) {
 			Thread.sleep(5000);
-			ResultSet rs = session
-					.execute("SELECT cql_version FROM system.local;");
+			ResultSet rs = session.execute(statement.bind(r.nextInt(1000000),
+					stringGenerator.nextSessionId(),
+					stringGenerator.nextSessionId(), r.nextInt(1000000),
+					r.nextInt(1000000)));
 			log.info(rs.toString());
 		}
+	}
+}
+
+final class SessionIdentifierGenerator {
+	private SecureRandom random = new SecureRandom();
+
+	public String nextSessionId() {
+		return new BigInteger(130, random).toString(10);
 	}
 }
